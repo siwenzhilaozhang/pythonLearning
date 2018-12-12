d02cfa52d6492114c1fd373d3d6a0a69cef8d2d9
cissy.zhang <cissy.zhang@shopee.com>
2018-12-12 23:01:42 +0800

    3.3列表

---
+++ basicPython/part3/date1212/date1212.py
@@ -0,0 +1,21 @@
+#3.3
+
+#3-8
+citys = ["xizang","chengdu","neimentgu","haerbing","xinjiang"]
+print citys
+print sorted(citys)
+print citys
+print sorted(citys,reverse=True)
+# citys1 = sorted(citys)
+# citys1.reverse()
+# print citys1
+print citys
+citys.reverse()
+print citys
+citys.reverse()
+print citys
+citys.sort()
+print citys
+citys.sort(reverse=True)
+print citys
+
