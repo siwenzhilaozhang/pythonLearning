2b780d46593b03d4be419bce5b2d8f11a343a7ae
cissy.zhang <cissy.zhang@shopee.com>
2018-12-18 23:25:36 +0800

    元组

---
+++ basicPython/part04/date1218/date1218.py
@@ -0,0 +1,11 @@
+#4.5
+
+#4-13
+print "4-13"
+foods = ("chaofan","chaomian","chaofen","tang","chaocai")
+for food in foods:
+    print food
+#foods[2]= "chaomi"
+foods = ("banmian","chaomian","chaofen","tangmian","chaocai")
+for food in foods:
+    print food
