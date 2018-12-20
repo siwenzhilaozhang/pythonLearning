c6c08f6f7e04755f304a47e96e59e4370b5eafcf
cissy.zhang <cissy.zhang@shopee.com>
2018-12-20 10:13:37 +0800

    字典

---
+++ basicPython/part05/date1219/date1219.py
@@ -0,0 +1,14 @@
+#5.3
+#5-3
+print "5-3"
+#alien_color = ["green","yellow","red"]
+#alien_color = "green"
+alien_color = "red"
+if alien_color == "green":
+    print "you get 5 records!"
+
+print "5-4"
+if alien_color == "green":
+    print "you get 5 records!"
+else:
+    print "you get 10 records!"
---
+++ basicPython/part06/date1220/date1220.py
@@ -0,0 +1,15 @@
+#6.2
+#6-1
+print "6-1"
+mylove = {
+    "first_name":"xu",
+    "last_name":"zhipeng",
+    "age":27,
+    "city":"shenzhen",
+}
+print mylove["first_name"]
+print mylove["last_name"]
+print mylove["age"]
+print mylove["city"]
+
+
